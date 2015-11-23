90232e7a0367693e4cddefec83f3387642147ea6
kishanp09 <kishanp09@gmail.com>
2015-11-23 17:03:28 -0500

    Kishan commit 11/23 5:03

--- app/src/main/java/com/example/kishan/stratmap/MapView.java
+++ app/src/main/java/com/example/kishan/stratmap/MapView.java
@@ -1,8 +1,16 @@
 package com.example.kishan.stratmap;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 
 import com.google.android.gms.location.LocationServices;
@@ -46,10 +54,10 @@ public class MapView extends FragmentActivity implements OnMapReadyCallback {
         //Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient):
         // Add a marker in Sydney and move the camera
         LatLng mylocation = new LatLng(42.2932850, -83.7138350);
-        mMap.addMarker(new MarkerOptions().position(mylocation).title("Hazard").snippet("Potholes").draggable(true));
+       // mMap.addMarker(new MarkerOptions().position(mylocation).title("Hazard").snippet("Potholes").draggable(true));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(mylocation));
         mMap.setMyLocationEnabled(true);
-        mMap.addMarker(new MarkerOptions()
+       /* mMap.addMarker(new MarkerOptions()
                 .position(mylocation)
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)).draggable(true).title("Hazard").snippet("Traffic"));
 
@@ -57,37 +65,80 @@ public class MapView extends FragmentActivity implements OnMapReadyCallback {
                 .position(mylocation)
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)).draggable(true).title("Hazard").snippet("Debris"));
 
-
+               */
     }
 
     public void PotholesPress(View view) {
-
-        LatLng location = new LatLng(50,50);
+        mMap.setMyLocationEnabled(true);
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double x = location.getLatitude();
+        double y = location.getLongitude();
+        LatLng mylocation = new LatLng(location.getLatitude(),location.getLongitude());
+        mMap.addMarker(new MarkerOptions()
+                .position(mylocation)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)).draggable(true).title("Hazard").snippet("Potholes"));
+        /* LatLng location = new LatLng(50, 50);
         mMap.addMarker(new MarkerOptions().position(location).title("Hazard").snippet("Potholes").draggable(true));
-
-
-
+        */
 
     }
 
-    public void TrafficPress(View view){
-
-        LatLng location = new LatLng(42,-90);
+    public void TrafficPress(View view) {
+        mMap.setMyLocationEnabled(true);
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double x = location.getLatitude();
+        double y = location.getLongitude();
+        LatLng mylocation = new LatLng(location.getLatitude(),location.getLongitude());
         mMap.addMarker(new MarkerOptions()
+                .position(mylocation)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)).draggable(true).title("Hazard").snippet("Traffic"));
+        //LatLng location = new LatLng(42, -90);
+       /* mMap.addMarker(new MarkerOptions()
                 .position(location)
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)).draggable(true).title("Hazard").snippet("Traffic"));
-
+*/
 
     }
 
-    public void DebrisPress(View view){
-        mMap.setMyLocationEnabled(true);
-        LatLng location = new LatLng(52,150);
+    @TargetApi(Build.VERSION_CODES.M)
+    public void DebrisPress(View view) {
+         mMap.setMyLocationEnabled(true);
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double x = location.getLatitude();
+        double y = location.getLongitude();
+        LatLng mylocation = new LatLng(location.getLatitude(),location.getLongitude());
         mMap.addMarker(new MarkerOptions()
-                .position(location)
+                .position(mylocation)
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)).draggable(true).title("Hazard").snippet("Debris"));
 
+        /*if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    public void requestPermissions(@NonNull String[] permissions, int requestCode)
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+
+
+        }
+        else {
+            Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            double x = location.getLatitude();
+            double y = location.getLongitude();
+            LatLng mylocation = new LatLng(location.getLatitude(),location.getLongitude());
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(mylocation));
+            mMap.addMarker(new MarkerOptions()
+                    .position(mylocation)
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)).draggable(true).title("Hazard").snippet("Debris"));
+        } */
+
+
+        }
 
 
     }
-}
